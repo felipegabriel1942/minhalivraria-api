cf6086626bc61282e7f7e868dd175122bed38d44
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2020-10-25 09:49:49 -0300

    Primeiro commit

---
+++ src/main/java/br/com/felipegabriel/minhalivraria/MinhalivrariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipegabriel.minhalivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhalivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhalivrariaApplication.class, args);
+	}
+
+}
